c973fafc4b61efb67737278a53ba43c3016a5519
kk200572 <kk200572@Gmail.com>
2024-09-27 20:24:56 +0800

    Update search.js

--- search.js
+++ search.js
@@ -17,14 +17,14 @@
             { keyword: '鸟', url: 'https://raw.githubusercontent.com/你的用户名/仓库名/分支名/圖片3.jpg' }
         ];
 
-        function searchImage() {
+       function searchImage() {
     const keyword = document.getElementById('search').value.trim();
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = ''; // 清空之前的搜索结果
 
-    // 创建一个正则表达式，确保关键字后面有空白或是字符串结束
-    const regex = new RegExp(`^${keyword}(\\s|$)`);
-    
+    // 创建一个正则表达式，确保关键字前后都是非字母字符（如空白）或字符串的开始和结束
+    const regex = new RegExp(`(^|\\s)${keyword}($|\\s)`);
+
     // 搜索图片并显示
     const filteredImages = imageDatabase.filter(image => regex.test(image.keyword));
     
